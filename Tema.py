6dd47a80c6da86b4aa8fb103cb807d141f4265cf
Sara <sarareyes1227@gmail.com>
2025-07-07 17:05:08 -0500

    Se agregan algunos metodos

---
+++ Tema.py
@@ -0,0 +1,9 @@
+# Tema
+
+class Tema:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.preguntas = []
+        
+def agregar_pregunta(self, pregunta):
+        self.preguntas.append(pregunta)
---
+++ main.py
@@ -0,0 +1,4 @@
+from encuestado import Encuestado
+from tema import Tema
+from pregunta import Pregunta
+
